b25cd0aabdf0ef7f45a4f289e28fdf6314765dcb
Jan Neumann <janneumannnneisen@gmail.com>
2023-03-01 20:02:02 +0100

    Externalizing the GameManager (WIP)

--- AGameOfMusic/Game1.cs
+++ AGameOfMusic/Game1.cs
@@ -6,9 +6,9 @@ namespace AGameOfMusic;
 
 public class Game1 : Game
 {
-    GameManager _gameManager;
     private GraphicsDeviceManager _graphics;
     private SpriteBatch _spriteBatch;
+    GameManager _gameManager;
 
     public Game1()
     {
@@ -20,20 +20,23 @@ public class Game1 : Game
     protected override void Initialize()
     {
         // TODO: Add your initialization logic here
-        _gameManager = new GameManager();
+        Globals.Content = Content;
         base.Initialize();
     }
 
     protected override void LoadContent()
     {
         _spriteBatch = new SpriteBatch(GraphicsDevice);
+        Globals.SpriteBatch = _spriteBatch;
         // TODO: use this.Content to load your game content here
+        _gameManager = new();
     }
 
     protected override void Update(GameTime gameTime)
     {
-        if (GamePad.GetState(PlayerIndex.One).Buttons.Back == ButtonState.Pressed || Keyboard.GetState().IsKeyDown(Keys.Escape))
-            Exit();
+        if (GamePad.GetState(PlayerIndex.One).Buttons.Back == ButtonState.Pressed || Keyboard.GetState().IsKeyDown(Keys.Escape)) Exit();
+        Globals.Update(gameTime);
+        _gameManager.Update();
         base.Update(gameTime);
     }
 
@@ -43,6 +46,7 @@ public class Game1 : Game
 
         // TODO: Add your drawing code here
         _spriteBatch.Begin();
+        _gameManager.Draw();
         _spriteBatch.End();
 
         base.Draw(gameTime);
